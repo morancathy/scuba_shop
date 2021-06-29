ded369ef7745c19227b398b35e58adef3a00bb66
Cat <morancathy@gmail.com>
2021-06-28 23:28:10 -0500

    updated mask index

--- views/MaskIndex.jsx
+++ views/MaskIndex.jsx
@@ -9,12 +9,13 @@ class MaskIndex extends React.Component {
       styles={[{key: 0, href: '/css/app.css'}, { key: 1, href: '/css/index.css'}]}>
         <div className="introDiv">
           <nav className="nav">
-            <a className="createLink" href="/masks/new">Create New Product</a><a className="cartLink" href="/masks">Cart</a>
+            <a className="createLink" href="/masks/new">Create New Product</a>
+            <a className="backLink" href="/underthesea">back to home page</a>
+            <a className="cartLink" href="/masks">Cart</a>
           </nav>
           <h1 className="h1">Under the Sea</h1>
           <h2 className="h2">Dive Shop</h2>
-          <p className="introP">Your neighboorhood Chicago-land dive shop. We've been in buisness for over 3 days! We offer
-          nothing but the best quality, prices and customer service.</p>
+          <h2>Masks</h2>
         </div>
         <div className="mainDiv">
           {
@@ -29,12 +30,6 @@ class MaskIndex extends React.Component {
               )
             })
           }
-        </div>
-        <div className="div2Tag">
-          <h1>what does this do</h1>
-        </div>
-        <div className="div3Tag">
-          <h1>Join Us!  Next excusion....El Nido, Philippines!</h1>
         </div>
         <footer><div>never stop exploring</div><div>© diesel 2021 </div></footer>
       </DefaultLayout>
