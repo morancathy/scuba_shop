80d85d39703e17ee5c0a01ab68a4a8d1544f55e7
Cat <morancathy@gmail.com>
2021-10-04 22:10:54 -0500

    mobile responsive

--- views/BootieEdit.jsx
+++ views/BootieEdit.jsx
@@ -7,27 +7,29 @@ class BootieEdit extends React.Component {
       <DefaultLayout
       title={"Edit Bootie"}
       styles={[{key: 0, href: '/css/app.css'}, { key: 1, href: '/css/edit.css'}]}>
-        <div className="div1">
-        </div>
-        <div className="div2">
-          <form action={`/booties/${this.props.bootie._id}?_method=PUT`} method="POST">
-            <h4>Name:</h4>
-              <input type="text" name="name" defaultValue={this.props.bootie.name}/><br/>
-            <h4>Description:</h4>
-              <textarea rows="4" cols="50" name="description" defaultValue={this.props.bootie.description}/><br/>
-            <h4>Image:</h4>
-              <input type="text" name="img" defaultValue={this.props.bootie.img}/><br/>
-            <h4>Price:</h4>
-              <input type="number" placeholder="0.00" step="0.01" min="0" name="price" defaultValue={this.props.bootie.price}/><br/>
-            <h4>Quantity:</h4>
-              <input type="number" name="qty" min="0" defaultValue={this.props.bootie.qty}/><br/><br/><br/>
-            <input id="submit" type="submit" name="" value="Submit edits" /><br/><br/>
-          </form>
-          <form id="deleteBut" method="POST" action={`/booties/${this.props.bootie._id}?_method=DELETE`}>
-            <input type="submit" value="Delete Bootie"/>
-          </form>
-        </div>
-        <div className="div3">
+        <div className="edit-div">
+          <div className="div1">
+          </div>
+          <div className="div2">
+            <form action={`/booties/${this.props.bootie._id}?_method=PUT`} method="POST">
+              <h4>Name:</h4>
+                <input type="text" name="name" defaultValue={this.props.bootie.name}/><br/>
+              <h4>Description:</h4>
+                <textarea rows="4" cols="20" name="description" defaultValue={this.props.bootie.description}/><br/>
+              <h4>Image:</h4>
+                <input type="text" name="img" defaultValue={this.props.bootie.img}/><br/>
+              <h4>Price:</h4>
+                <input type="number" placeholder="0.00" step="0.01" min="0" name="price" defaultValue={this.props.bootie.price}/><br/>
+              <h4>Quantity:</h4>
+                <input type="number" name="qty" min="0" defaultValue={this.props.bootie.qty}/><br/><br/><br/>
+              <input id="submit" type="submit" name="" value="Submit edits" /><br/><br/>
+            </form>
+            <form id="deleteBut" method="POST" action={`/booties/${this.props.bootie._id}?_method=DELETE`}>
+              <input type="submit" value="Delete Bootie"/>
+            </form>
+          </div>
+          <div className="div3">
+          </div>
         </div>
       </DefaultLayout>
     )
--- views/MaskEdit.jsx
+++ views/MaskEdit.jsx
@@ -7,27 +7,29 @@ class MaskEdit extends React.Component {
       <DefaultLayout
       title={"Edit Mask"}
       styles={[{key: 0, href: '/css/app.css'}, { key: 1, href: '/css/edit.css'}]}>
-        <div className="div1">
-        </div>
-        <div className="div2">
-          <form action={`/masks/${this.props.mask._id}?_method=PUT`} method="POST">
-            <h4>Name:</h4>
-              <input type="text" name="name" defaultValue={this.props.mask.name}/><br/>
-            <h4>Description:</h4>
-              <textarea rows="4" cols="50" name="description" defaultValue={this.props.mask.description}/><br/>
-            <h4>Image:</h4>
-              <input type="text" name="img" defaultValue={this.props.mask.img}/><br/>
-            <h4>Price:</h4>
-              <input type="number" placeholder="0.00" step="0.01" min="0" name="price" defaultValue={this.props.mask.price}/><br/>
-            <h4>Quantity:</h4>
-              <input type="number" name="qty" min="0" defaultValue={this.props.mask.qty}/><br/><br/><br/>
-            <input id="submit" type="submit" name="" value="Submit edits" /><br/><br/>
-          </form>
-          <form id="deleteBut" method="POST" action={`/masks/${this.props.mask._id}?_method=DELETE`}>
-            <input type="submit" value="Delete Mask"/>
-          </form>
-        </div>
-        <div className="div3">
+        <div className="edit-div">
+          <div className="div1">
+          </div>
+          <div className="div2">
+            <form action={`/masks/${this.props.mask._id}?_method=PUT`} method="POST">
+              <h4>Name:</h4>
+                <input type="text" name="name" defaultValue={this.props.mask.name}/><br/>
+              <h4>Description:</h4>
+                <textarea rows="4" cols="20" name="description" defaultValue={this.props.mask.description}/><br/>
+              <h4>Image:</h4>
+                <input type="text" name="img" defaultValue={this.props.mask.img}/><br/>
+              <h4>Price:</h4>
+                <input type="number" placeholder="0.00" step="0.01" min="0" name="price" defaultValue={this.props.mask.price}/><br/>
+              <h4>Quantity:</h4>
+                <input type="number" name="qty" min="0" defaultValue={this.props.mask.qty}/><br/><br/><br/>
+              <input id="submit" type="submit" name="" value="Submit edits" /><br/><br/>
+            </form>
+            <form id="deleteBut" method="POST" action={`/masks/${this.props.mask._id}?_method=DELETE`}>
+              <input type="submit" value="Delete Mask"/>
+            </form>
+          </div>
+          <div className="div3">
+          </div>
         </div>
       </DefaultLayout>
     )
